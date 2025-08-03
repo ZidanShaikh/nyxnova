8012e62507c72e211ea70af144e8eefe5af9fe73
ZidanShaikh <zidanyt10@gmail.com>
2025-08-03 18:57:36 +0530

    home.html

--- B/js/lock.js
+++ B/js/lock.js
@@ -2,5 +2,5 @@ const unlockDate = new Date("2025-08-07");
 const today = new Date();
 
 if (today >= unlockDate) {
-  window.location.href = "home.html";
+  window.location.href = "https://nyxnova.netlify.app/home.html";
 }
